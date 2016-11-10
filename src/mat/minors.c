726fef3e833cf4c587b997f552037ef3340b44e4
Guillaume Pinchon <gpinchon@debian>
2016-11-10 17:05:43 +0100

    Cleaned up some functions, added tests for MAT2

--- src/mat/minors.c
+++ src/mat/minors.c
@@ -6,7 +6,7 @@
 /*   By: gpinchon <gpinchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 01:10:48 by gpinchon          #+#    #+#             */
-/*   Updated: 2016/11/10 15:35:17 by gpinchon         ###   ########.fr       */
+/*   Updated: 2016/11/10 16:43:21 by gpinchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,95 +22,60 @@ t_mat2	mat2_minors(const t_mat2 m)
 
 t_mat3	mat3_minors(const t_mat3 m)
 {
-	float	det[9];
+	t_mat3	r;
 
-	det[0] = mat2_determinant((t_mat2){.m = {m.m[4], m.m[5], m.m[7], m.m[8]}});
-	det[1] = mat2_determinant((t_mat2){.m = {m.m[3], m.m[5], m.m[6], m.m[8]}});
-	det[2] = mat2_determinant((t_mat2){.m = {m.m[3], m.m[4], m.m[6], m.m[7]}});
-	det[3] = mat2_determinant((t_mat2){.m = {m.m[1], m.m[2], m.m[7], m.m[8]}});
-	det[4] = mat2_determinant((t_mat2){.m = {m.m[0], m.m[2], m.m[6], m.m[8]}});
-	det[5] = mat2_determinant((t_mat2){.m = {m.m[0], m.m[1], m.m[6], m.m[7]}});
-	det[6] = mat2_determinant((t_mat2){.m = {m.m[1], m.m[2], m.m[4], m.m[5]}});
-	det[7] = mat2_determinant((t_mat2){.m = {m.m[0], m.m[2], m.m[3], m.m[5]}});
-	det[8] = mat2_determinant((t_mat2){.m = {m.m[0], m.m[1], m.m[3], m.m[4]}});
-	return ((t_mat3){.m = {
-		det[0], det[1], det[2],
-		det[3], det[4], det[5],
-		det[6], det[7], det[8]}});
+	r.m[0] = mat2_determinant((t_mat2){.m = {m.m[4], m.m[5], m.m[7], m.m[8]}});
+	r.m[1] = mat2_determinant((t_mat2){.m = {m.m[3], m.m[5], m.m[6], m.m[8]}});
+	r.m[2] = mat2_determinant((t_mat2){.m = {m.m[3], m.m[4], m.m[6], m.m[7]}});
+	r.m[3] = mat2_determinant((t_mat2){.m = {m.m[1], m.m[2], m.m[7], m.m[8]}});
+	r.m[4] = mat2_determinant((t_mat2){.m = {m.m[0], m.m[2], m.m[6], m.m[8]}});
+	r.m[5] = mat2_determinant((t_mat2){.m = {m.m[0], m.m[1], m.m[6], m.m[7]}});
+	r.m[6] = mat2_determinant((t_mat2){.m = {m.m[1], m.m[2], m.m[4], m.m[5]}});
+	r.m[7] = mat2_determinant((t_mat2){.m = {m.m[0], m.m[2], m.m[3], m.m[5]}});
+	r.m[8] = mat2_determinant((t_mat2){.m = {m.m[0], m.m[1], m.m[3], m.m[4]}});
+	return (r);
 }
 
-t_mat4	mat4_minors(const t_mat4 m)
+static void	mat4_minors1(t_mat4 *r, t_mat4 m)
 {
-	float	det[16];
+	r->m[8] = mat3_determinant((t_mat3){.m = {m.m[1], m.m[2], m.m[3],
+		m.m[5], m.m[6], m.m[7], m.m[13], m.m[14], m.m[15]}});
+	r->m[9] = mat3_determinant((t_mat3){.m = {m.m[0], m.m[2], m.m[3],
+		m.m[4], m.m[6], m.m[7], m.m[12], m.m[14], m.m[15]}});
+	r->m[10] = mat3_determinant((t_mat3){.m = {m.m[0], m.m[1], m.m[3],
+		m.m[4], m.m[5], m.m[7], m.m[12], m.m[13], m.m[15]}});
+	r->m[11] = mat3_determinant((t_mat3){.m = {m.m[0], m.m[1], m.m[2],
+		m.m[4], m.m[5], m.m[6], m.m[12], m.m[13], m.m[14]}});
+	r->m[12] = mat3_determinant((t_mat3){.m = {m.m[1], m.m[2], m.m[3],
+		m.m[5], m.m[6], m.m[7], m.m[9], m.m[10], m.m[11]}});
+	r->m[13] = mat3_determinant((t_mat3){.m = {m.m[0], m.m[2], m.m[3],
+		m.m[4], m.m[6], m.m[7], m.m[8], m.m[10], m.m[11]}});
+	r->m[14] = mat3_determinant((t_mat3){.m = {m.m[0], m.m[1], m.m[3],
+		m.m[4], m.m[5], m.m[7], m.m[8], m.m[9], m.m[11]}});
+	r->m[15] = mat3_determinant((t_mat3){.m = {m.m[0], m.m[1], m.m[2],
+		m.m[4], m.m[5], m.m[6], m.m[8], m.m[9], m.m[10]}});
+}
 
-	det[0] = mat3_determinant((t_mat3){.m = {
-		m.m[5], m.m[6], m.m[7],
-		m.m[9], m.m[10], m.m[11],
-		m.m[13], m.m[14], m.m[15]}});
-	det[1] = mat3_determinant((t_mat3){.m = {
-		m.m[4], m.m[6], m.m[7],
-		m.m[8], m.m[10], m.m[11],
-		m.m[12], m.m[14], m.m[15]}});
-	det[2] = mat3_determinant((t_mat3){.m = {
-		m.m[4], m.m[5], m.m[7],
-		m.m[8], m.m[9], m.m[11],
-		m.m[12], m.m[13], m.m[15]}});
-	det[3] = mat3_determinant((t_mat3){.m = {
-		m.m[4], m.m[5], m.m[6],
-		m.m[8], m.m[9], m.m[10],
-		m.m[12], m.m[13], m.m[14]}});
-	det[4] = mat3_determinant((t_mat3){.m = {
-		m.m[1], m.m[2], m.m[3],
-		m.m[9], m.m[10], m.m[11],
-		m.m[13], m.m[14], m.m[15]}});
-	det[5] = mat3_determinant((t_mat3){.m = {
-		m.m[0], m.m[2], m.m[3],
-		m.m[8], m.m[10], m.m[11],
-		m.m[12], m.m[14], m.m[15]}});
-	det[6] = mat3_determinant((t_mat3){.m = {
-		m.m[0], m.m[1], m.m[3],
-		m.m[8], m.m[9], m.m[11],
-		m.m[12], m.m[13], m.m[15]}});
-	det[7] = mat3_determinant((t_mat3){.m = {
-		m.m[0], m.m[1], m.m[2],
-		m.m[8], m.m[9], m.m[10],
-		m.m[12], m.m[13], m.m[14]}});
-	det[8] = mat3_determinant((t_mat3){.m = {
-		m.m[1], m.m[2], m.m[3],
-		m.m[5], m.m[6], m.m[7],
-		m.m[13], m.m[14], m.m[15]}});
-	det[9] = mat3_determinant((t_mat3){.m = {
-		m.m[0], m.m[2], m.m[3],
-		m.m[4], m.m[6], m.m[7],
-		m.m[12], m.m[14], m.m[15]}});
-	det[10] = mat3_determinant((t_mat3){.m = {
-		m.m[0], m.m[1], m.m[3],
-		m.m[4], m.m[5], m.m[7],
-		m.m[12], m.m[13], m.m[15]}});
-	det[11] = mat3_determinant((t_mat3){.m = {
-		m.m[0], m.m[1], m.m[2],
-		m.m[4], m.m[5], m.m[6],
-		m.m[12], m.m[13], m.m[14]}});
-	det[12] = mat3_determinant((t_mat3){.m = {
-		m.m[1], m.m[2], m.m[3],
-		m.m[5], m.m[6], m.m[7],
-		m.m[9], m.m[10], m.m[11]}});
-	det[13] = mat3_determinant((t_mat3){.m = {
-		m.m[0], m.m[2], m.m[3],
-		m.m[4], m.m[6], m.m[7],
-		m.m[8], m.m[10], m.m[11]}});
-	det[14] = mat3_determinant((t_mat3){.m = {
-		m.m[0], m.m[1], m.m[3],
-		m.m[4], m.m[5], m.m[7],
-		m.m[8], m.m[9], m.m[11]}});
-	det[15] = mat3_determinant((t_mat3){.m = {
-		m.m[0], m.m[1], m.m[2],
-		m.m[4], m.m[5], m.m[6],
-		m.m[8], m.m[9], m.m[10]}});
-	return ((t_mat4){ .m = {
-		det[0], det[1], det[2], det[3],
-		det[4], det[5], det[6], det[7],
-		det[8], det[9], det[10], det[11],
-		det[12], det[13], det[14], det[15],
-	}});
+t_mat4		mat4_minors(const t_mat4 m)
+{
+	t_mat4	r;
+
+	r.m[0] = mat3_determinant((t_mat3){.m = {m.m[5], m.m[6], m.m[7],
+		m.m[9], m.m[10], m.m[11],m.m[13], m.m[14], m.m[15]}});
+	r.m[1] = mat3_determinant((t_mat3){.m = {m.m[4], m.m[6], m.m[7],
+		m.m[8], m.m[10], m.m[11], m.m[12], m.m[14], m.m[15]}});
+	r.m[2] = mat3_determinant((t_mat3){.m = {m.m[4], m.m[5], m.m[7],
+		m.m[8], m.m[9], m.m[11], m.m[12], m.m[13], m.m[15]}});
+	r.m[3] = mat3_determinant((t_mat3){.m = {m.m[4], m.m[5], m.m[6],
+		m.m[8], m.m[9], m.m[10], m.m[12], m.m[13], m.m[14]}});
+	r.m[4] = mat3_determinant((t_mat3){.m = {m.m[1], m.m[2], m.m[3],
+		m.m[9], m.m[10], m.m[11], m.m[13], m.m[14], m.m[15]}});
+	r.m[5] = mat3_determinant((t_mat3){.m = {m.m[0], m.m[2], m.m[3],
+		m.m[8], m.m[10], m.m[11], m.m[12], m.m[14], m.m[15]}});
+	r.m[6] = mat3_determinant((t_mat3){.m = {m.m[0], m.m[1], m.m[3],
+		m.m[8], m.m[9], m.m[11], m.m[12], m.m[13], m.m[15]}});
+	r.m[7] = mat3_determinant((t_mat3){.m = {m.m[0], m.m[1], m.m[2],
+		m.m[8], m.m[9], m.m[10], m.m[12], m.m[13], m.m[14]}});
+	mat4_minors1(&r, m);
+	return (r);
 }
--- test/test.c
+++ test/test.c
@@ -6,13 +6,26 @@
 /*   By: gpinchon <gpinchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 13:19:24 by gpinchon          #+#    #+#             */
-/*   Updated: 2016/11/10 15:46:10 by gpinchon         ###   ########.fr       */
+/*   Updated: 2016/11/10 17:03:36 by gpinchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <vml.h>
+
+void	mat2_print(MAT2 m)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		printf("%c%f\t", !(i % 2) ? '\n' : 0, m.m[i]);
+		i++;
+	}
+	printf("\n\n");
+}
 
 void	mat3_print(MAT3 m)
 {
@@ -25,6 +38,37 @@ void	mat3_print(MAT3 m)
 		i++;
 	}
 	printf("\n\n");
+}
+
+void	mat4_print(MAT4 m)
+{
+	int	i;
+
+	i = 0;
+	while (i < 16)
+	{
+		printf("%c%f\t", !(i % 4) ? '\n' : 0, m.m[i]);
+		i++;
+	}
+	printf("\n\n");
+}
+
+void	mat2_assert(MAT2 m, MAT2 m1)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (!float_equal(m.m[i], m1.m[i]))
+		{
+			printf("Assertion failed at index : %i\n\
+Value : %f, Expected : %f\n", i, m.m[i], m1.m[i]);
+			exit(-42);
+		}
+		i++;
+	}
+	printf("Verification passed !\n\n");
 }
 
 void	mat3_assert(MAT3 m, MAT3 m1)
@@ -45,19 +89,6 @@ Value : %f, Expected : %f\n", i, m.m[i], m1.m[i]);
 	printf("Verification passed !\n\n");
 }
 
-void	mat4_print(MAT4 m)
-{
-	int	i;
-
-	i = 0;
-	while (i < 16)
-	{
-		printf("%c%f\t", !(i % 4) ? '\n' : 0, m.m[i]);
-		i++;
-	}
-	printf("\n\n");
-}
-
 void	mat4_assert(MAT4 m, MAT4 m1)
 {
 	int	i;
@@ -76,10 +107,86 @@ Value : %f, Expected : %f\n", i, m.m[i], m1.m[i]);
 	printf("Verification passed !\n\n");
 }
 
-/*1	-1	0	4
-6	1	3	10
-7	15	-2	5
-20	9	8	1*/
+void	test_mat2()
+{
+	MAT2	matrix;
+	MAT2	temp;
+
+	printf("Da matrix :\n");
+	mat2_print(matrix = (MAT2){.m = {
+			1, -2,
+			6, 3
+		}});
+	printf("Determinant :\n");
+	printf("\n%f\n\n", mat2_determinant(matrix));
+	printf("Minors :\n");
+	mat2_print(temp = mat2_minors(matrix));
+	mat2_assert(temp, (MAT2){.m = {
+		3, 6,
+		-2, 1
+	}});
+	printf("Cofactors :\n");
+	mat2_print(temp = mat2_cofactor(matrix));
+	mat2_assert(temp, (MAT2){.m = {
+		3, -6,
+		2, 1
+	}});
+	printf("Adjugate :\n");
+	mat2_print(temp = mat2_adjugate(matrix));
+	mat2_assert(temp, (MAT2){.m = {
+		3, 2,
+		-6, 1
+	}});
+	printf("Inverse :\n");
+	mat2_print(temp = mat2_inverse(matrix));
+	mat2_assert(temp, (MAT2){.m = {
+		3/15.f, 2/15.f,
+		-6/15.f, 1/15.f,
+	}});
+}
+
+void	test_mat3()
+{
+	MAT3	matrix;
+	MAT3	temp;
+
+	printf("Da matrix :\n");
+	mat3_print(matrix = (MAT3){.m = {
+			1, -1, 0,
+			6, 1, 3,
+			7, 15, -2
+		}});
+	printf("Determinant :\n");
+	printf("\n%f\n\n", mat3_determinant(matrix));
+	printf("Minors :\n");
+	mat3_print(temp = mat3_minors(matrix));
+	mat3_assert(temp, (MAT3){.m = {
+		-47, -33, 83,
+		2, -2, 22,
+		-3, 3, 7
+	}});
+	printf("Cofactors :\n");
+	mat3_print(temp = mat3_cofactor(matrix));
+	mat3_assert(temp, (MAT3){.m = {
+		-47, 33, 83,
+		-2, -2, -22,
+		-3, -3, 7
+	}});
+	printf("Adjugate :\n");
+	mat3_print(temp = mat3_adjugate(matrix));
+	mat3_assert(temp, (MAT3){.m = {
+		-47, -2, -3,
+		33, -2, -3,
+		83, -22, 7
+	}});
+	printf("Inverse :\n");
+	mat3_print(temp = mat3_inverse(matrix));
+	mat3_assert(temp, (MAT3){.m = {
+		47/80.f, 2/80.f, 3/80.f,
+		-33/80.f, 2/80.f, 3/80.f,
+		-83/80.f, 22/80.f, -7/80.f
+	}});
+}
 
 void	test_mat4()
 {
@@ -129,52 +236,10 @@ void	test_mat4()
 	}});
 }
 
-void	test_mat3()
-{
-	MAT3	matrix;
-	MAT3	temp;
-
-	printf("Da matrix :\n");
-	mat3_print(matrix = (MAT3){.m = {
-			1, -1, 0,
-			6, 1, 3,
-			7, 15, -2
-		}});
-	printf("Determinant :\n");
-	printf("\n%f\n\n", mat3_determinant(matrix));
-	/*printf("Minors :\n");
-	mat3_print(temp = mat3_minors(matrix));
-	mat3_assert(temp, (MAT3){.m = {
-		1428, 987, -2457, 21,
-		594, 342, -1071, 234,
-		1, -125, -147, -31,
-		-223, -97, 147, -80
-	}});*/
-	printf("Cofactors :\n");
-	mat3_print(temp = mat3_cofactor(matrix));
-	mat3_assert(temp, (MAT3){.m = {
-		-47, 33, 83,
-		-2, -2, -22,
-		-3, -3, 7
-	}});
-	printf("Adjugate :\n");
-	mat3_print(temp = mat3_adjugate(matrix));
-	mat3_assert(temp, (MAT3){.m = {
-		-47, -2, -3,
-		33, -2, -3,
-		83, -22, 7
-	}});
-	printf("Inverse :\n");
-	mat3_print(temp = mat3_inverse(matrix));
-	mat3_assert(temp, (MAT3){.m = {
-		47/80.f,	2/80.f,	3/80.f,
-		-33/80.f,	2/80.f,	3/80.f,
-		-83/80.f,	22/80.f,	-7/80.f
-	}});
-}
-
 int main(void)
 {
+	printf("\n\nCHECKING MAT2\n");
+	test_mat2();
 	printf("\n\nCHECKING MAT3\n");
 	test_mat3();
 	printf("\n\nCHECKING MAT4\n");
