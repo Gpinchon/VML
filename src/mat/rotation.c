4c66984013fa33af4eedc89d54dd4e8fffab778e
Guillaume Pinchon <Guillaume Pinchon>
2016-11-24 23:45:07 +0100

    Corrected rotation matrix creation

--- src/mat/rotation.c
+++ src/mat/rotation.c
@@ -6,7 +6,7 @@
 /*   By: gpinchon <gpinchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 17:56:16 by gpinchon          #+#    #+#             */
-/*   Updated: 2016/11/10 18:04:03 by gpinchon         ###   ########.fr       */
+/*   Updated: 2016/11/24 23:43:49 by gpinchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,28 @@ t_mat2	mat2_rotation(float angle)
 
 t_mat3	mat3_rotation(t_vec3 axis)
 {
-	t_vec3	c;
-	t_vec3	s;
+	VEC3	c;
+	VEC3	s;
 
-	c = (t_vec3){cos(axis.x), cos(axis.y), cos(axis.z)};
-	s = (t_vec3){cos(axis.x), cos(axis.y), cos(axis.z)};
-	return ((t_mat3){.m = {
-		c.y * c.z, c.z * s.x * s.y, c.x * c.z * s.y + s.x * s.z,
-		c.y * s.z, c.x * c.z + s.x * s.y * s.z, -c.z * s.x + c.x * s.y * s.x,
-		-s.y, c.y * s.x, c.x * c.y
-	}});
+	c = (VEC3){cos(axis.x), cos(axis.y), cos(axis.z)};
+	s = (VEC3){sin(axis.x), sin(axis.y), sin(axis.z)};
+	return (mat3_mult_mat3(mat3_mult_mat3(
+		(MAT3){.m = {
+		1, 0, 0,
+		0, c.x, -s.x,
+		0, s.x, c.x
+		}}, (MAT3){.m = {
+		c.y, 0, s.y,
+		0, 1, 0,
+		-s.y, 0, c.y
+		}}), (MAT3){.m = {
+		c.z, -s.z, 0,
+		s.z, c.z, 0,
+		0, 0, 1
+	}}));
 }
 
 t_mat4	mat4_rotation(t_vec3 axis)
 {
-	t_vec3	c;
-	t_vec3	s;
-
-	c = (t_vec3){cos(axis.x), cos(axis.y), cos(axis.z)};
-	s = (t_vec3){cos(axis.x), cos(axis.y), cos(axis.z)};
-	return ((t_mat4){.m = {
-		c.y * c.z, c.z * s.x * s.y, c.x * c.z * s.y + s.x * s.z, 0,
-		c.y * s.z, c.x * c.z + s.x * s.y * s.z, -c.z * s.x + c.x * s.y * s.x, 0,
-		-s.y, c.y * s.x, c.x * c.y, 0,
-		0, 0, 0, 1
-	}});
+	return (mat3_to_mat4(mat3_rotation(axis)));
 }
