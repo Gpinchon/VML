339156dd665a3f107aec28905249ecb854f645ed
Guillaume Pinchon <Guillaume Pinchon>
2016-11-25 17:41:09 +0100

    Corrected cylinder intersection

--- src/ray/primitives.c
+++ src/ray/primitives.c
@@ -6,7 +6,7 @@
 /*   By: gpinchon <gpinchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 19:25:25 by gpinchon          #+#    #+#             */
-/*   Updated: 2016/11/13 15:11:28 by gpinchon         ###   ########.fr       */
+/*   Updated: 2016/11/25 17:40:55 by gpinchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,9 +59,10 @@ INTERSECT	intersect_sphere(t_primitive s, t_ray r)
 ** (Other than creating a "sphere" along the cylinder's axis...)
 */
 
-INTERSECT	intersect_cylinder(t_primitive cp, t_ray r)
+INTERSECT intersect_cylinder(t_primitive cp, t_ray r)
 {
-	t_vec3		v[6];
+	t_vec3	v[6];
+	float	f[4];
 	INTERSECT	inter;
 
 	inter = new_intersect();
@@ -71,16 +72,21 @@ INTERSECT	intersect_cylinder(t_primitive cp, t_ray r)
 	v[3] = vec3_proj(r.direction, cp.direction);
 	v[4] = vec3_sub(r.direction, v[3]);
 	v[5] = vec3_sub(vec3_sub(r.origin, v[2]), v[1]);
+	f[0] = vec3_dot(v[4], v[4]);
+	f[1] = vec3_dot(v[5], v[4]) * 2.0;
+	f[2] = vec3_dot(v[5], v[5]) - (cp.radius2);
+	if ((f[3] = (f[1] * f[1]) - (4.0 * f[0] * f[2])) <= FLOAT_ZERO)
+		return (inter);
+	f[3] = sqrt(f[3]);
+	inter.distance[0] = (-f[1] - f[3]) / (2.0 * f[0]);
+	inter.distance[1] = (-f[1] + f[3]) / (2.0 * f[0]);
 	if (cp.size > 0 && vec3_length(vec3_sub(vec3_add(v[2],
 		vec3_scale(v[3], inter.distance[0])), v[0])) >= cp.size / 2)
 		inter.distance[0] = 0;
 	if (cp.size > 0 && vec3_length(vec3_sub(vec3_add(v[2],
 		vec3_scale(v[3], inter.distance[1])), v[0])) >= cp.size / 2)
 		inter.distance[1] = 0;
-	if (!(inter.intersects = solve_quadratic(
-				vec3_dot(v[4], v[4]), vec3_dot(v[4], v[4]) * 2.0,
-				vec3_dot(v[5], v[5]) - (cp.radius2),
-				inter.distance) && intersect_test(inter.distance)))
+	if (!(inter.intersects = intersect_test(inter.distance)))
 		return (inter);
 	inter.position = intersect_compute_position(r, inter.distance[0]);
 	inter.normal = cylinder_normal(inter.position, cp);
