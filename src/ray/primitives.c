ae7e1cfaa37b1adf2d28bef0f555c05df11f3451
Guillaume PINCHON <gpinchon@e3r7p10.42.fr>
2017-02-13 18:50:16 +0100

    corrected triangle intersection

--- src/ray/primitives.c
+++ src/ray/primitives.c
@@ -6,36 +6,48 @@
 /*   By: gpinchon <gpinchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 19:25:25 by gpinchon          #+#    #+#             */
-/*   Updated: 2017/02/10 14:30:54 by mbarbari         ###   ########.fr       */
+/*   Updated: 2017/02/13 18:49:21 by gpinchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vml.h>
 
-INTERSECT			intersect_triangle(OBJ p, t_ray r, TRANSFORM *tr)
+static inline VEC3	compute_tp(OBJ p, t_ray r, TRANSFORM *tr, VEC3 *tp)
 {
-	INTERSECT	i;
-	float		n_dot_r;
-	VEC3		tp[3];
-
-	i = new_intersect();
 	tp[0] = mat4_mult_vec3(tr->transform, p.triangle.point[0]);
 	tp[1] = mat4_mult_vec3(tr->transform, p.triangle.point[1]);
 	tp[2] = mat4_mult_vec3(tr->transform, p.triangle.point[2]);
-	i.normal = vec3_cross(vec3_sub(tp[1], tp[0]), vec3_sub(tp[2], tp[0]));
-	n_dot_r = vec3_dot(i.normal, r.direction);
-	if (fabs(n_dot_r) < FLOAT_ZERO || (i.distance[0] =
-	(vec3_dot(i.normal, r.origin) + vec3_dot(i.normal, tp[0])) / n_dot_r) < 0)
+	tp[1] = vec3_sub(tp[1], tp[0]);
+	tp[2] = vec3_sub(tp[2], tp[0]);
+	tp[0] = vec3_sub(r.origin, tp[0]);
+	return (vec3_cross(r.direction, tp[2]));
+}
+
+INTERSECT			intersect_triangle(OBJ p, t_ray r, TRANSFORM *tr)
+{
+	VEC3		tp[3];
+	VEC3		qvec;
+	VEC3		pvec;
+	VEC3		fact;
+	INTERSECT	i;
+
+	i = new_intersect();
+	pvec = compute_tp(p, r, tr, tp);
+	fact.z = 1 / vec3_dot(tp[1], pvec);
+	fact.x = vec3_dot(tp[0], pvec) * fact.z;
+	if (fact.x < 0 || fact.x > 1)
 		return (i);
-	i.distance[1] = i.distance[0];
+	qvec = vec3_cross(tp[0], tp[1]);
+	fact.y = vec3_dot(r.direction, qvec) * fact.z;
+	if (fact.y < 0 || fact.x + fact.y > 1)
+		return (i);
+	i.distance[0] = vec3_dot(tp[2], qvec) * fact.z;
+	if (i.distance[0] <= 0)
+		return (i);
 	i.position = intersect_compute_position(r, i.distance[0]);
-	if (vec3_dot(i.normal, vec3_cross(vec3_sub(tp[1], tp[0]),
-		vec3_sub(i.position, tp[0]))) < 0
-	|| vec3_dot(i.normal, vec3_cross(vec3_sub(tp[2], tp[1]),
-		vec3_sub(i.position, tp[1]))) < 0
-	|| vec3_dot(i.normal, vec3_cross(vec3_sub(tp[0], tp[2]),
-		vec3_sub(i.position, tp[2]))) < 0)
-		return (i);
+	i.normal = (VEC3){tp[1].y * tp[2].z - tp[1].z * tp[2].y,
+						tp[1].z * tp[2].x - tp[1].x * tp[2].z,
+						tp[1].x * tp[2].y - tp[1].y * tp[2].x};
 	i.intersects = 1;
 	return (i);
 }
